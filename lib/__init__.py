f2964e1f24243dbeb6dd120d6588aeec34fc80d7
BKrone <kr1.brandon@gmail.com>
2022-07-31 16:14:26 -0500

    Updating .lib __init__

--- lib/__init__.py
+++ lib/__init__.py
@@ -1,6 +1,9 @@
 from .vector import *
 from .vector import __all__ as __vectorall__
 
+from .matrix import *
+from .matrix import __all__ as __matrixall__
+
 from .line import *
 from .line import __all__ as __lineall__
 
@@ -10,10 +13,23 @@ from .plane import __all__ as __planeall__
 from .kinematics import *
 from .kinematics import __all__ as __kinematicsall__
 
+from .face import *
+from .face import __all__ as __faceall__
+
+from .body import *
+from .body import __all__ as __bodyall__
+
+from .interpolate import *
+from .interpolate import __all__ as __interpolateall__
+
 
 __all__  = (
     *__vectorall__,
+    *__matrixall__,
     *__lineall__,
     *__planeall__,
-    *__kinematicsall__
+    *__kinematicsall__,
+    *__faceall__,
+    *__bodyall__,
+    *__interpolateall__
     )
