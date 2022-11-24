a97fe894bf299f4205f0fe73f745c807be7eb66d
BKrone <kr1.brandon@gmail.com>
2022-11-24 11:59:00 -0600

    Adding Scene types.

---
+++ lib/scene/__init__.py
@@ -0,0 +1,7 @@
+from .scene import *
+from .scene import __all__ as __sceneall__
+
+
+__all__ = (
+    *__sceneall__,
+    )
--- lib/scene/scene.py
+++ lib/scene/scene.py
@@ -1,6 +1,11 @@
+from ..element.abc import ElementTypeABC, MutableMasterElementType
 
 
-class Scene:
-    def __init__(self):
-        self._cameras = []
-        self._faces = []
+class SceneABC(ElementTypeABC, MutableMasterElementType):
+    def __init__(self, origin, basis, components):
+        ElementTypeABC.__init__(self, origin, basis)
+        MutableMasterElementType.__init__(self, components)
+
+    def refresh(self):
+        [component.refresh() for component in self._components]
+        return self
